93d16855e2de5a88877a6eb0cdcf36585f0b6278
Aryt Nosliw <arytnosliwdeb134@outlook.com>
2022-10-08 09:22:09 -0400

    bullet class created, enemy sprite created

--- assets.py
+++ assets.py
@@ -1,14 +1,48 @@
 
 import pygame
 
-class Player(pygame.sprite.Sprite):
+class Player():
 
-    def __init__(self, start_x, start_y,image):
+    def __init__(self, start_x, start_y, image):
 
-        super().__init__()
- 
         self.image = image
         self.rect = self.image.get_rect(center=(start_x, start_y))
         self.player_x_change = 0.3
+        self.player_y_change = 0.3
 
- 
+    def draw(self, screen, x, y):
+
+        screen.blit(self.image, (x,y))
+
+class Bullet():
+
+    def __init__(self):
+
+        self.bullet_state = False
+        self.bullet_y_change = 8
+        self.bulletY = 0
+    
+    def fire_bullet(self, screen, x, y, bulletimg):
+
+        self.bullet_state = True
+
+        # draw bullet onto the screen
+
+        screen.blit(bulletimg, (x+10,y+10))
+
+class Enemy:
+
+    def __init__(self, enemyimg, x, y):
+        self.image = enemyimg
+        self.rect = self.image.get_rect(center=(x, y))
+        self.enemy_x_change = 0.3
+        self.enemy_y_change = 0.3
+    
+
+    def draw(self, screen, x, y):
+    
+        screen.blit(self.image, (x,y))
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import pygame
 from assets import Player
+from assets import Bullet
+from assets import Enemy
 
 pygame.init()
 
@@ -9,17 +11,40 @@ background = pygame.image.load("images/space-background.jpg")
 
 playerimg = pygame.image.load("images/spaceship.png")
 
+bulletimg = pygame.image.load("images/bullet.png")
+
+enemyimg = pygame.image.load("images/enemy.png")
+enemyimg = pygame.transform.scale(enemyimg, (50,50))
+
 screen = pygame.display.set_mode((800,600))
 
 running = True
 
+start_x = screen.get_rect().center[0]
+start_y =  screen.get_rect().center[1] + 200
+
 # Player
-active_sprites_list = pygame.sprite.Group()
+player = Player(start_x, start_y, playerimg)
 
-player = Player(screen.get_rect().center[0], screen.get_rect().center[1] + 200, playerimg)
 
-all_sprites = pygame.sprite.Group([player])
+enemy_start_x = 0
+enemy_start_y = 20
 
+# Enemy
+enemy = Enemy(enemyimg, enemy_start_x, enemy_start_y)
+
+playerX = start_x
+playerY = start_y
+
+global player_x_change
+player_x_change = 0
+
+global player_y_change
+player_y_change = 0
+
+# Bullet
+bullet = Bullet()
+bullet.bulletY = start_x
 
 while running:
 
@@ -29,10 +54,35 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
 
-    all_sprites.update(screen)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                player_x_change = player.player_x_change
+            if event.key == pygame.K_LEFT:
+                player_x_change = -(player.player_x_change)
+            if event.key == pygame.K_UP:
+                player_y_change = -(player.player_y_change)
+            if event.key == pygame.K_DOWN:
+                player_y_change = player.player_y_change
+            if event.key == pygame.K_SPACE:
+                bullet.fire_bullet(screen, playerX, bullet.bulletY, bulletimg)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+                player_x_change = 0
+                player_y_change = 0
 
-    all_sprites.draw(screen)
+    if bullet.bulletY <= 0:
+        bullet.bulletY = start_x
+        bullet.bullet_state = False
+
+    if bullet.bullet_state:
+        bullet.fire_bullet(screen, playerX, bullet.bulletY, bulletimg)
+        bullet.bulletY -= bullet.bullet_y_change
+
+    playerY += player_y_change
+    playerX += player_x_change
+
+    player.draw(screen, playerX, playerY)
+    enemy.draw(screen, enemy_start_x, enemy_start_y)
 
     pygame.display.update()
