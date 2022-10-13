293e15c2a9875aabf44f01992d4fd04dba27a68f
Aryt Nosliw <arytnosliwdeb134@outlook.com>
2022-10-13 14:21:04 -0400

    final rough draft

--- assets.py
+++ assets.py
@@ -11,10 +11,14 @@ class Player():
         self.rect = self.image.get_rect(center=(start_x, start_y))
         self.player_x_change = 1
         self.player_y_change = 1
+        self.shield = False
 
     def draw(self, screen, x, y):
 
         screen.blit(self.image, (x,y))
+
+        if self.shield == True:
+            pygame.draw.circle(screen, (255,255,255), (x+25,y+25), 50, width=5) # Here <<<
 
 class Bullet():
 
@@ -25,11 +29,13 @@ class Bullet():
         self.bulletY = 0
         self.bulletX = 0
         self.fired = True
+        self.ammo = 200
 
     
     def fire_bullet(self, screen, x, y, bulletimg):
 
         self.bullet_state = True
+        self.ammo -= 0.1
 
         screen.blit(bulletimg, (x+10,y+10))
     
@@ -45,7 +51,7 @@ class Bullet():
 
 class Enemy:
 
-    def __init__(self, screen, enemyimg, x, y):
+    def __init__(self, screen, enemyimg, x, y, attack):
 
         self.image = enemyimg
         self.enemy_x_change = 0.3
@@ -54,11 +60,41 @@ class Enemy:
         self.enemyY = y
         self.screen = screen
         self.alive = True
+        self.attacking_enemy = attack
+        self.bulletimg = pygame.image.load("images/enemy-bullet.png")
+        self.bulletimg = pygame.transform.rotate(self.bulletimg, 180)
+        self.bulletimg = pygame.transform.scale(self.bulletimg , (50,50))
+        self.enemy_bullet_x = self.enemyX
+        self.enemy_bullet_y = self.enemyY
+        self.enemy_bullet_y_change = 2
 
     def draw(self):
 
         if self.alive:
             self.screen.blit(self.image, (self.enemyX, self.enemyY))
+            self.screen.blit(self.image, (self.enemyX, self.enemyY))
+
+    def enemy_bullet_movement(self):
+
+        if self.attacking_enemy:
+
+            self.enemy_bullet_y += self.enemy_bullet_y_change
+
+            self.screen.blit(self.bulletimg, (self.enemy_bullet_x+7 ,  self.enemy_bullet_y+25))
+
+            if self.enemy_bullet_y >= 800:
+                self.enemy_bullet_x = self.enemyX
+                self.enemy_bullet_y = self.enemyY
+
+    def damage(self, playerX, playerY):
+
+        distance = math.sqrt(math.pow(playerX-self.enemy_bullet_x,2) + (math.pow(playerY-self.enemy_bullet_y,2)))
+
+        if distance < 18:
+            return True
+        else:
+            return False
+
 
     def movement(self):
 
@@ -71,6 +107,7 @@ class Enemy:
             self.enemy_x_change = -abs(self.enemy_x_change)
             self.enemyY += self.enemy_y_change 
 
+
 class Display:
 
     def __init__(self, screen):
@@ -78,6 +115,20 @@ class Display:
         self.textX = 10
         self.textY = 10
         self.screen = screen
+        self.total_health = 200
+        self.health_bar_w = 200
+        self.health_bar_h = 20
+        self.hit = False
+        self.shield = False
+        self.ammo_bar_w = 200
+        self.ammo_bar_h = 20
+    
+    def show_timer(self, timer):
+
+        font = pygame.font.Font('freesansbold.ttf', 32)
+
+        time = font.render(timer, True, (255,255,255))
+        self.screen.blit(time, (self.textX, self.textY+50))
 
     def show_score(self):
 
@@ -92,35 +143,110 @@ class Display:
 
         game_over = font.render("GAME OVER", True, (255,255,255))
         self.screen.blit(game_over, (200, 270))
+    
+    def health_bar_status(self):
+
+        if self.hit and self.shield == False:
+
+            if self.total_health <= 200:
+                self.total_health -= 1
+                self.hit = False
+        else:
+            pass
+
+        pygame.draw.rect(self.screen,(255,0,0),(5,550, self.total_health, self.health_bar_h))
+        pygame.draw.rect(self.screen,(255,255,255),(5, 550, self.health_bar_w, self.health_bar_h),4)
+
+    def ammo_bar_starus(self, total_ammo):
+
+        pygame.draw.rect(self.screen,(255,255,0),(5,510, total_ammo, self.ammo_bar_h))
+        pygame.draw.rect(self.screen,(255,255,255),(5, 510, self.ammo_bar_w, self.ammo_bar_h),4)
 
 class Powerups:
 
-    def __init__(self, screen, image):
+    def __init__(self, screen, fast_img, heart_img, shield_img):
 
         self.screen = screen
-        self.image = image
+        self.fast_img = fast_img
+        self.heart_img = heart_img
+        self.shield_img = shield_img
         self.fast_player_x =  random.randint(0,735)
         self.fast_player_y = random.randint(50,150)
+        self.heart_player_x =  random.randint(0,735)
+        self.heart_player_y = random.randint(50,150)
         self.fast_player_y_change = 0.3
+        self.shield_player_x =  random.randint(0,735)
+        self.shield_player_y = random.randint(50,150)
+        self.fast_powerup = False
+        self.heart_powerup = False
+        self.shield_powerup = False
+
     
-    def respawn_fast_powerup(self):
+    def respawn_powerups(self):
         self.fast_player_x =  random.randint(0,735)
         self.fast_player_y = random.randint(50,150)
+        self.heart_player_x = random.randint(0,735)
+        self.heart_player_y = random.randint(50,150)
+        self.shield_player_x = random.randint(0,735)
+        self.shield_player_y = random.randint(50,150)
+        self.fast_powerup = False
+        self.heart_powerup = False
+        self.shield_powerup = False
 
-    def spawn_fast_powerup(self):
+    def fast_powerup_movement(self):
 
         self.fast_player_y += self.fast_player_y_change
 
-        self.screen.blit(self.image, (self.fast_player_x, self.fast_player_y))
+        self.screen.blit(self.fast_img, (self.fast_player_x, self.fast_player_y))
 
-    def fast_powerup(self, playerX, playerY):
+    def heart_powerup_movement(self):
 
-        distance = math.sqrt(math.pow(playerX-self.fast_player_x,2) + (math.pow(playerY-self.fast_player_y,2)))
+        self.heart_player_y += self.fast_player_y_change
+
+        # Health Powerup
+        self.screen.blit(self.heart_img, (self.heart_player_x, self.heart_player_y))
+    
+    def shield_powerup_movement(self):
+
+        self.shield_player_y += self.fast_player_y_change
+
+        # Health Powerup
+        self.screen.blit(self.shield_img, (self.shield_player_x, self.shield_player_y))
+
+    def get_powerup(self, playerX, playerY, powerup):
+
+        match powerup:
+            case "fast":
+                distance = math.sqrt(math.pow(playerX-self.fast_player_x,2) + (math.pow(playerY-self.fast_player_y,2)))
+            case "heart":
+                distance = math.sqrt(math.pow(playerX-self.heart_player_x,2) + (math.pow(playerY-self.heart_player_y,2)))
+            case "sheild":
+                distance = math.sqrt(math.pow(playerX-self.shield_player_x,2) + (math.pow(playerY-self.shield_player_y,2)))
 
         if distance < 18:
             return True
         else:
             return False
+
+    def define_powerups(self):
+
+        rand_num = random.randint(0,30)
+
+        if rand_num%2 == 0:
+            self.fast_powerup = True
+        else:
+            self.fast_powerup = False
+            
+        if rand_num%3 == 0:
+            self.heart_powerup = True
+        else:
+            self.heart_powerup = False
+
+        if rand_num%6 == 0:
+            self.shield_powerup = True
+        else:
+            self.shield_powerup = False
+      
 
         
         
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 
+from asyncio import shield
 from distutils.spawn import spawn
-from re import S
+from re import M, S
 import pygame
 from assets import Player
 from assets import Bullet
 from assets import Enemy
 from assets import Display
 from assets import Powerups
+
 import random
 import math
+import datetime
+import time
+from time import perf_counter
 
 pygame.init()
+t1_start = perf_counter()
+
+clock = pygame.time.Clock()
+
+counter = 0
 
 # Backgound Image
 background = pygame.image.load("images/space-background.jpg")
@@ -23,9 +33,19 @@ enemyimg = pygame.image.load("images/enemy.png")
 
 enemyimg = pygame.transform.scale(enemyimg, (50,50))
 
+enemy_spaceship_img = pygame.image.load("images/enemy-spaceship.png")
+enemy_spaceship_img = pygame.transform.scale(enemy_spaceship_img, (50,50))
+
 fast_powerup_img = pygame.image.load("images/flash.png")
 
 fast_powerup_img = pygame.transform.scale(fast_powerup_img, (50,50))
+
+heart_powerup_img = pygame.image.load("images/heart.png")
+
+heart_powerup_img = pygame.transform.scale(heart_powerup_img, (50,50))
+
+shield_img = pygame.image.load("images/shield.png")
+shield_img = pygame.transform.scale(shield_img, (50,50))
 
 
 screen = pygame.display.set_mode((800,600))
@@ -50,10 +70,13 @@ enemy_obj_list = []
 def spawn_enemies():
 
     # Enemy
-    for i in range(num_of_enemies):
+    for i in range(int(num_of_enemies/2)):
 
         # don't have the enemies near each other
-        enemy_obj_list.append(Enemy(screen, enemyimg, random.randint(0,735), random.randint(50,150)))
+        enemy_obj_list.append(Enemy(screen, enemyimg, random.randint(0,735), random.randint(50,150), False))
+
+    for i in range(int(num_of_enemies/2)):
+        enemy_obj_list.append(Enemy(screen, enemy_spaceship_img, random.randint(0,735), random.randint(50,150), True))
 
 spawn_enemies()
 spawn_count = 0
@@ -79,14 +102,27 @@ count = 0
 
 # Powerups
 
-powerup = Powerups(screen, fast_powerup_img)
-fast_powerup = False
+powerup = Powerups(screen, fast_powerup_img, heart_powerup_img, shield_img)
+
+powerup.define_powerups()
+
+miliseconds = pygame.time.get_ticks()
+start_seconds = pygame.time.get_ticks()
+
+trigger = pygame.USEREVENT + 0
+pygame.time.set_timer(trigger, 1000)
+
+miliseconds = pygame.time.get_ticks() - start_seconds
+timer = str(datetime.timedelta(seconds=(round(miliseconds/1000))))[3:]
 
 while running:
 
     screen.blit(background, (0,0))
 
     for event in pygame.event.get():
+        if event.type == trigger:
+            miliseconds = pygame.time.get_ticks() - start_seconds
+            timer = str(datetime.timedelta(seconds=(round(miliseconds/1000))))[3:]
         if event.type == pygame.QUIT:
             running = False
 
@@ -99,11 +135,14 @@ while running:
                 player_y_change = -(player.player_y_change)
             if event.key == pygame.K_DOWN:
                 player_y_change = player.player_y_change
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and bullet.ammo > 0:
                 bullet.bulletX = playerX
                 bullet.bulletY = playerY
                 bullet.fire_bullet(screen,  playerX, playerY, bulletimg)
                 bullet.fired = True
+            if event.key == pygame.K_r:
+                # Reload bullets
+                bullet.ammo = 200
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                 player_x_change = 0
@@ -140,6 +179,10 @@ while running:
 
         enemy_obj_list[count].draw()
         enemy_obj_list[count].movement()
+        enemy_obj_list[count].enemy_bullet_movement()
+
+        if enemy_obj_list[count].damage(playerX, playerY):
+            display.hit = True
 
         if enemy_obj_list[count].alive == False:
             enemy_obj_list.pop(count)
@@ -147,31 +190,78 @@ while running:
             count = 0
         else:
             count += 1
+
        
     count = 0
 
     if not enemy_obj_list:
 
-        fast_powerup = True
+        # Get the coordinates
+        powerup.respawn_powerups()
 
-        if grand_num_of_enemies < 15:
+        # Revert back to default
+        if powerup.fast_powerup == False:
+            # powerup.fast_powerup = True
+            player.player_x_change = 1
+            player.player_y_change = 1
+        if powerup.shield_powerup == False:
+            # powerup.shield_powerup = True
+            display.shield = False
+            player.shield = False
+
+        powerup.define_powerups()
+
+        if grand_num_of_enemies < 16:
             grand_num_of_enemies += 2
             num_of_enemies = grand_num_of_enemies
-            print("spawn")
             spawn_enemies()
-        else:
+        if grand_num_of_enemies >= 16:
+            powerup.fast_powerup = False
+            powerup.heart_powerup = False
+            powerup.shield_powerup = False
             display.game_over_text()
 
-    if fast_powerup:
-        
-        powerup.spawn_fast_powerup()
+    if display.total_health <= 0:
 
-        if powerup.fast_powerup(playerX, playerY):
+        display.game_over_text()
 
-            player.player_x_change = 2
-            player.player_y_change = 2
-            fast_powerup = False
-            powerup.respawn_fast_powerup()
+    if powerup.fast_powerup:
+
+        powerup.fast_powerup_movement()
+
+        # Powerup collision
+        if powerup.get_powerup(playerX, playerY, "fast"):
+
+            player.player_x_change = 4
+            player.player_y_change = 4
+            powerup.fast_powerup = False
+
+    if powerup.heart_powerup:
+
+        powerup.heart_powerup_movement()
+
+        if powerup.get_powerup(playerX, playerY,"heart"):
+            display.total_health = 200
+            powerup.heart_powerup = False
+
+    if powerup.shield_powerup:
+
+        powerup.shield_powerup_movement()
+
+        if powerup.get_powerup(playerX, playerY, "sheild"):
+            
+            powerup.shield_powerup = False
+
+            # Sheild is on, so don't deduct health
+            display.shield = True
+            player.shield = True
+
 
     display.show_score()
+    display.health_bar_status()
+    display.ammo_bar_starus(bullet.ammo)
+    display.show_timer(timer)
     pygame.display.update()
+
+
+# when an enemy becomes a frenemy it crosses the path of other enemies and kills them
